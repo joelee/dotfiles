51da581609ba784980ad2416e0afe6224a9f44ed
Joseph Lee <joel@joeworks.com>
2026-01-27 15:38:01 +0000

    Fix GetSecrets script

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -2,5 +2,6 @@
 
 type get-secret.sh >/dev/null 2>&1 && exit
 
+mkdir -p "${HOME}/.local/bin" >/dev/null 2>&1
 cp "${HOME}/.local/share/chezmoi/private_dot_local/bin/executable_get-secret.sh" "${HOME}/.local/bin/get-secret.sh"
 chmod +x "${HOME}/.local/bin/get-secret.sh"
